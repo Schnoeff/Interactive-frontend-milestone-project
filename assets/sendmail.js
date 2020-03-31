a6333a071e7deff9e6c847105f3f046db382164f
Schnoeff <02bburrell@live.co.uk>
2020-03-31 14:21:39 +0000

    started to add basic padding to hotel descriptions

--- assets/sendmail.js
+++ assets/sendmail.js
@@ -1,5 +1,5 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "SeasideRetreats", {
+    emailjs.send("gmail", "Seaside Retreats", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "enquiry": contactForm.enquiry.value
